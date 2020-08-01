fd33065fcf11632c40b2c7e8f3258675cbe2f103
Mae <maeberris@outlook.com>
2020-08-01 17:51:43 -0400

    completed Exercise 2

--- src/components/TweetContext.js
+++ src/components/TweetContext.js
@@ -1,11 +1,14 @@
 import React from "react";
 import avatar from "../assets/carmen-sandiego.png";
+import moment from "moment";
 export const TweetContext = React.createContext(null);
 
 export const TweetProvider = ({ children }) => {
+  const date = moment().format("h:mm a - MMMM do, YYYY");
   return (
     <TweetContext.Provider
       value={{
+        date,
         tweetContents: "Where in the world am I?",
         displayName: "Carmen Sandiego ✨",
         username: "carmen-sandiego",
